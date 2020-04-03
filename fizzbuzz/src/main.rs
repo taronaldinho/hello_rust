2d658b8d713445655b59cfb26bd8f083f0b739fe
taronaldinho <kotaro.yamashita@gmail.com>
2020-04-03 18:01:36 +0900

    一時保存

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,141 @@
+// Re:FizzBuzzから始めるRust生活 - Qiita
+// https://qiita.com/hinastory/items/543ae9749c8bccb9afbc
+
+fn main() {
+    fizz_buzz_8(100);
+}
+
+
+
+
+fn fizz_buzz_0(limit: i32) {
+    let mut i: i32 = 1;                // i は ミュータブルな値に束縛する変数名なので mut を付ける。
+                                       // ここで型アノテーションがなくてもよい。 (Rust の型推論がはたらく)
+
+    loop {                             // loop による無限ループ。
+        if i % 3 == 0 && i % 5 == 0 {  // if による条件分岐処理。&& 演算子で複数条件 AND で判断する。
+            println!("FizzBuzz");      // || 演算子で OR となる。
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+        if i == limit {                // 無限ループを抜けるための条件分岐処理。
+            break;                     // break でループを抜ける。
+        }
+        i += 1;                        // カウントアップ。
+    }
+}
+
+
+fn fizz_buzz_1(limit: i32) {
+    let mut i = 1;                     // Rust の型推論を利用。
+
+    while i <= limit {                 // while による継続判断付きのループ処理。
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", i);
+        }
+        i += 1;
+    }
+}
+
+
+fn fizz_buzz_2(limit: i32) {
+    for x in 1..limit+1 {              // for in による集合要素ひとつひとつに対する処理。
+        if x % 3 == 0 && x % 5 == 0 {  // これによりカウンタおよびカウントアップが不要になる。
+            println!("FizzBuzz");      // また、スライス表記 .. で範囲 (整数値の集合) を表している。
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }        
+    }
+}
+
+fn fizz_buzz_3(limit: i32) {
+    for x in 1..=limit {                     // ..=endpoint で終端を含む集合に。
+        match x % 15 {                       // match を利用して x の 15 による剰余を分類してマッチ。
+            0              => println!("FizzBuzz"),  // 単式パターン
+            3 | 6 | 9 | 12 => println!("Fizz"),      // 複式パターン
+            5 | 10         => println!("Buzz"),
+            _              => println!("{}", x),     // _ を用いるとワイルドカードパターンになる。
+        }                                    // ひとつの match のアーム (=>) は同じ型でなければならない。
+    }
+}
+
+
+fn fizz_buzz_4(limit: i32) {
+    for x in 1..=limit {
+        match x {
+            e if e % 15 == 0 => println!("FizzBuzz"), // パターンの列挙をしない方法。
+            e if e % 3 == 0  => println!("Fizz"),      // if 以降はパターンガードと呼ばれる。
+            e if e % 5 == 0  => println!("Buzz"),
+            e                => println!("{}", e),
+        }
+    }
+}
+
+
+fn fizz_buzz_5(limit: i32) {
+    for x in 1..=limit {
+        match (x % 3, x % 5) {            // タプルとのマッチング。
+        (0, 0) => println!("FizzBuzz"), 
+        (0, _) => println!("Fizz"),     
+        (_, 0) => println!("Buzz"),     
+        _      => println!("{}", x),
+        }
+    }
+}
+
+
+fn fizz_buzz_6(limit: i32) {
+    for x in 1..=limit {
+        let s = match (x % 3, x % 5) {        // match は式であるため、評価された値を返す。
+            (0, 0) => "FizzBuzz".to_string(), // (ここではそれを s に代入する)
+            (0, _) => "Fizz".to_string(),     // "" で囲われたリテラルは &str (文字列スライス型の参照) 
+            (_, 0) => "Buzz".to_string(),     // であり、to_string() によって　String 型となる。
+            _      => x.to_string(),
+        };                                    // let キーワードを使って代入するとき、末尾に ; が必要。
+        println!("{}", s);
+    }
+}
+
+
+fn fizz_buzz_7(limit: i32) {
+    for x in 1..=limit {
+        let tmp;                      // 後述。
+        let s = match (x % 3, x % 5) {
+            (0, 0) => "FizzBuzz",     // &str
+            (0, _) => "Fizz",         // &str
+            (_, 0) => "Buzz",         // &str
+            _      => {
+                tmp = x.to_string();  // String 型である tmp の参照は Rust の型強制によって &str になる。
+                &tmp                  // このアームを抜けても参照が有効でなければならないので、前述の行での
+            },                        // tmp の宣言が必要になる。
+        };
+        println!("{}", s);
+    }
+}
+
+
+fn fizz_buzz_8(limit: i32) {
+    let fz = |x: i32| {       // クロージャ (無名関数) の定義
+        match (x % 3, x % 5) {
+            (0, 0) => format!("FizzBuzz"),
+            (0, _) => format!("Fizz"),
+            (_, 0) => format!("Buzz"),
+                _  => x.to_string(),
+        }
+    };
+    (1..=limit).map(fz).for_each(|x| println!("{}", x)); // 高階関数とクロージャの利用
+}
--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,6 +1,7 @@
 // ← スラッシュ 2 つはコメント
 // 標準 (std) ライブラリから io ライブラリをインポート
-// Cargo.io からダウンロードした rand 
+// 同じく cmp をインポート
+// Cargo.io からダウンロードした rand をインポート
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
@@ -8,37 +9,36 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
     // 1 から 101 未満 の範囲の整数からランダムに 1 つ抜き出す。
-    // この時点で型推論により Rust は secret_number を i32 型と推定する
-    // i32 は符号付きの 32 bit 整数である
+    // この時点で型推論により Rust は secret_number を i32 型と推定する。
+    // i32 は符号付きの 32 bit 整数である。
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
-
+    
     // loop で繰り返し
     loop {
 
         println!("Please input your guess.");
 
         // ミュータブルな guess という変数を作成し、
-        // String 型の関連関数 new() が返す新しい String インスタンスに束縛する
+        // String 型の関連関数 new() が返す新しい String インスタンスに束縛する。
         // (関連関数-associated function: Static Method のようなもの)
-        // この時点で型推論により Rust は guess を String 型と推定する
+        // この時点で型推論により Rust は guess を String 型と推定する。
         let mut guess = String::new();
 
-        // io ライブラリの関連関数 stdin() は標準入力へのハンドルを返す
-        // さらにハンドルに対して read_line() メソッドを呼んで文字列への
-        // ミュータブルな参照 (&mut) を渡す
-        // 同メソッドは io::Result という値 (Ok/Err) も返す
-        // expect() メソッドは呼び出された値が成功を表すものでなければ、
-        // 与えたメッセージとともに panic! する
+        // io ライブラリの関連関数 stdin() は標準入力へのハンドルを返す。
+        // さらにハンドルに対して read_line() メソッドを呼んで文字列へのミュータブルな参照 (Refernce) を渡す。
+        // これにより、所有権は guess のままである。→ 借用 (Borrowing)
+        // 同メソッドは io::Result という値 (Ok/Err) も返す。
+        // expect() メソッドは呼び出された値が成功を表すものでなければ与えたメッセージとともに panic! する。
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line!");
 
-        // String 型の guess から前後の余分なスペースや改行文字を除いて 
-        // 符号なし 32 bit 整数の u32 型にキャストする (シャドーイング)
-        // parse() は Result を返します。
-        // Ok(num) は Ok をアンラップして得られた値(整数値)を num という
-        // 名前に設定し、続く右側でその値をそのまま返している
+        // String 型の guess から前後の余分なスペースや改行文字を除いて符号なし 32 bit 整数の 
+        // u32 型にキャストする。→ シャドーイング (Shadowing)
+        // parse() は Result を返す。
+        // Ok(num) は Ok をアンラップして得られた値(整数値)を num という名前に設定し、
+        // 続く右側でその値をそのまま返している。
         let guess: u32 = match guess.trim().parse() {
                     Ok(num) => num,
                     Err(_) => continue,
@@ -46,20 +46,17 @@ fn main() {
 
         println!("You guessed: {}", guess);
 
-        // guess と secret_number (の参照) を比較する
-        // u32 型の guess と比較されることにより、secret_number も 
-        // u32 型と推測される
-        // cmp() は比較可能な全てのものに対して呼べるメソッドで、引数として、
-        // 比較したい相手の参照を取ります。 そして、先ほど use した、
-        // Ordering 型の値を返します。 
-        // match 文を使って、正確に Ordering のどれであるかを判断しています。 
-        // Ordering は enum (列挙型) で、enumは「enumeration(列挙)」の略です
+        // guess と secret_number (の参照) を比較する。
+        // u32 型の guess と比較されることにより、secret_number も u32 型と推測される。
+        // cmp() は比較可能な全てのものに対して呼べるメソッドで、引数として、比較したい相手の参照を取る。 
+        // そして、use した Ordering 型の値を返す。 
+        // match 文を使って、正確に Ordering のどれであるかを判断しています。         
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
-                // break で loop から抜ける
+                // break で loop から抜ける。
                 break;
                 }
             }
