6127f93b1fd2da5371eff2c7640fb0d9c4c9f560
taronaldinho <kotaro.yamashita@gmail.com>
2020-05-14 06:09:48 +0900

    一時保存

---
+++ bitonic_soter/src/third.rs
@@ -0,0 +1,112 @@
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x, true),
+            SortOrder::Decending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!("The length of x is not a power of two. (x.len(): {}", x.len()))
+    }
+    
+}
+
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
+    }
+}
+
+fn sub_sort<T: Ord>(x: &mut [T], up: bool) {
+    compare_and_swap(x, up);
+    if x.len() > 1 {
+        let mid_point = x.len() / 2;
+        sub_sort(&mut x[..mid_point], up);
+        sub_sort(&mut x[mid_point..], up);
+    }
+}
+
+fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
+    let mid_point = x.len() / 2;
+    for i in 0..mid_point {
+        if (x[i] > x[mid_point + i]) == up {
+            x.swap(i, mid_point + i);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::sort;
+    use crate::SortOrder::*;
+
+    struct Student {
+        first_name: String,
+        last_name: String,
+        age: u8,
+    }
+    impl Student {
+        fn new(first_name: &str, last_name: &str, age: u8) -> Self {
+            first_name: first_name.to_string(),
+            last_name: last_name.to_string(),
+            age,
+        }
+    }
+
+    #[test]
+    fn sort_u32_ascending() {
+        let mut x:Vec<u32>  = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
+    }
+
+    #[test]
+    fn sort_u32_decending() {
+        let mut x: Vec<u32>  = vec![10, 30, 11, 20, 4, 330, 21, 110];
+        assert_eq!(sort(&mut x, &Decending), Ok(()));
+        assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+    }
+
+    #[test]
+    fn sort_str_ascending() {
+        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
+        assert_eq!(sort(&mut x, &Ascending), Ok(()));
+        assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
+    }
+
+    #[test]
+    fn sort_str_decending() {
+        let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
+        assert_eq!(sort(&mut x, &Decending), Ok(()));
+        assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"])
+    }
+
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11];
+        assert!(sort(&mut x, &Ascending).is_err());
+    }
+
+    #[test]
+    fn sort_students_by_age_ascending() {
+        let taro = Student::new("Taro", "Yamada", 16);
+        let hanako = Student::new("Hanako", "Yamada", 14);
+        let kyoko = Student::new("Kyoko", "Ito", 15);
+        let ryosuke = Student::new("Ryosuke", "Hayashi", 17);
+
+        let mut x = vec![&taro, &hanako, &kyoko, &ryosuke];
+        let expected = vec![&hanako, &kyoko, &taro, &ryosuke];
+
+        assert_eq!(
+            &mut x, &|a, b| a.age.cmp(&b.age),
+            Ok(())
+        );
+        assert_eq!(x, expected);
+
+    }
+}
